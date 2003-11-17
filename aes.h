57c96a87e3d219830967b6b07c0360a905e8de3a
luis <luis>
2003-11-17 11:30:34 +0000

    Modificaciones en los tipos de datos para comenzar a manejar el estado de cifrado, etc. (comienzo)

--- aes.h
+++ aes.h
@@ -1,4 +1,4 @@
-/* $Id: aes.h,v 1.6 2003/11/14 23:59:18 luis Exp $
+/* $Id: aes.h,v 1.7 2003/11/17 11:30:34 luis Exp $
  * Author: Luis Colorado <Luis.Colorado@HispaLinux.ES>
  * Date: Tue Nov 11 00:25:04 MET 2003
  *
@@ -24,14 +24,29 @@
 #ifndef AES_H
 #define AES_H
 
-static char AES_H_RCSId[] = "\n$Id: aes.h,v 1.6 2003/11/14 23:59:18 luis Exp $\n";
+static char AES_H_RCSId[] = "\n$Id: aes.h,v 1.7 2003/11/17 11:30:34 luis Exp $\n";
 
 /* constants */
 #define AES_POL 0x11b
+#define AES_WS	4
+#define AES_MAX_Nb	8
+#define AES_MAX_Nk	8
+#define AES_MAX_Nr	14
+#define AES_MAX_Nrk	(AES_MAX_Nb*(AES_MAX_Nr+1))
 
 /* types */
 typedef unsigned int AES_BYTE;
 typedef unsigned char BYTE;
+typedef BYTE AES_STATE[AES_MAX_Nb][AES_WS];
+typedef BYTE AES_ROUNDS[AES_MAX_Nrk][AES_WS];
+
+typedef struct AES_State {
+	size_t Nb;
+	size_t Nk;
+	size_t Nr;
+	AES_STATE State;
+	AES_ROUNDS RoundKey;
+} AES_State;
 
 /* prototypes */
 AES_BYTE aes_mult(AES_BYTE x, AES_BYTE y, AES_BYTE pol); /* test ok */
@@ -47,4 +62,4 @@ void aes_InvSubBytes(BYTE *b, size_t n);
  * protected against double inclusion from other files.
  */
 
-/* $Id: aes.h,v 1.6 2003/11/14 23:59:18 luis Exp $ */
+/* $Id: aes.h,v 1.7 2003/11/17 11:30:34 luis Exp $ */
